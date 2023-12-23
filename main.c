b5dda1605c2ce4af2a2fc4dffc7ea8ca68120b20
Basabjeet-Deb <154667994+Basabjeet-Deb@users.noreply.github.com>
2023-12-23 15:14:03 +0530

    Triangle Pattern Printer
    This code prints the Triangle pattern to as many rows the user needs.

--- main.c
+++ main.c
@@ -3,10 +3,13 @@ int main() {
    int i, space, rows, k = 0;
    printf("Enter the number of rows: ");
    scanf("%d", &rows);
+      //first loop is working for the rows 
    for (i = 1; i <= rows; ++i, k = 0) {
+      //second loop working for the spaces 
       for (space = 1; space <= rows - i; ++space) {
          printf("  ");
       }
+      //the while loop is for printing the symbol
       while (k != 2 * i - 1) {
          printf("* ");
          ++k;
